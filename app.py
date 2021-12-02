fd0cfcf7bfd11dbd6c16885f80fdb2af2345661d
HardyBright <hardy_bright@brown.edu>
2021-12-02 01:01:43 +0000

    Second Commit

--- app.py
+++ app.py
@@ -2,12 +2,21 @@ import dash
 from dash import dash_table
 from dash import dcc # dash core components
 from dash import html
-
+from dash.dependencies import Input, Output, State
 import pandas as pd
 
 df = pd.read_csv('https://bit.ly/elements-periodic-table')
 
 app = dash.Dash(__name__)
+
+def identity(x): return x
+
+pivot = df.pivot_table(
+  index='Period',
+  columns='Group', 
+  values='Element',
+  aggfunc=identity,
+)
 
 app.layout = html.Div([
     dcc.Dropdown(
@@ -23,16 +32,12 @@ app.layout = html.Div([
 
     html.Div(dash_table.DataTable(
         id='table',
-        columns=[{"name": i, "id": i} for i in df.columns],
-        data=df.to_dict('records'),
+        columns=[{"name": str(i), "id": str(i)} for i in pivot.columns], 
+        data=pivot.to_dict('records'),
+
     ))
 ])
-@app.callback(
-    Output,
-    Input(component_id='OK!', component_property='n_clicks'),
-    State(component_id='index', component_property='value'),
-    State(component_id='column', component_property='value'),
-    State(component_id='values', component_property='value'),
-)
+
+
 
 app.run_server(debug=True, host="0.0.0.0")
